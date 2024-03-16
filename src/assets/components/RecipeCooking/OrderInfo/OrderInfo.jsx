1b8eb70522e7137407b658745b108266215a4954
saifullah-10 <saifullah.bus@gmail.com>
2024-03-16 21:31:37 +0600

    update functionality

--- src/assets/components/RecipeCooking/OrderInfo/OrderInfo.jsx
+++ src/assets/components/RecipeCooking/OrderInfo/OrderInfo.jsx
@@ -1,6 +1,13 @@
 import "./order.css";
 const OrderInfo = ({ orders, handleCurrentlyCook, currents }) => {
-  // console.log(recipe_name);
+  const totalPreparingTime = currents.reduce(
+    (total, current) => total + current.preparing_time,
+    0
+  );
+  const totalCalories = currents.reduce(
+    (total, current) => total + current.calories,
+    0
+  );
   return (
     <>
       <div>
@@ -22,31 +29,6 @@ const OrderInfo = ({ orders, handleCurrentlyCook, currents }) => {
                     <th></th>
                   </tr>
                 </thead>
-                {/* <tbody>
-                  {orders.length === 0 ? (
-                    <tr>
-                      <td colSpan="5">No orders to display</td>
-                    </tr>
-                  ) : (
-                    orders.map((order, index) => (
-                      <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{order.recipe_name}</td>
-                        <td>{order.preparing_time} min</td>
-                        <td>{order.calories} cal</td>
-                        <td>
-                          <button
-                            onClick={() => handleCurrentlyCook(order)}
-                            className="px-3 py-1 bg-[#0BE58A] rounded-full"
-                          >
-                            Preparing
-                          </button>
-                        </td>
-                      </tr>
-                    ))
-                  )}
-                </tbody> */}
-
                 <tbody>
                   {orders.length !== 0 ? (
                     orders.map((order, index) => (
@@ -90,16 +72,30 @@ const OrderInfo = ({ orders, handleCurrentlyCook, currents }) => {
                 <tbody>
                   {currents.length !== 0 ? (
                     currents.map((current, index) => (
-                      <tr>
+                      <tr key={index}>
                         <td>{index + 1}</td>
                         <td>{current.recipe_name}</td>
-                        <td>20 minutes</td>
-                        <td>400 calories</td>
+                        <td>{current.preparing_time} min</td>
+                        <td>{current.calories} cal</td>
                       </tr>
                     ))
                   ) : (
                     <tr className="h-20">
                       <td colSpan="5">No Orders To Display</td>
+                    </tr>
+                  )}
+                  {currents.length !== 0 && (
+                    <tr className="border-t-4 border-black">
+                      <td></td>
+                      <td></td>
+                      <td>
+                        Total Time <br />
+                        {totalPreparingTime} minutes
+                      </td>
+                      <td>
+                        Total Calories <br />
+                        {totalCalories} calories
+                      </td>
                     </tr>
                   )}
                 </tbody>
